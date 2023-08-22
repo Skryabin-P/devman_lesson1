1f7c0b9356f72c94e5e9c2c931db118bc9c5fe3d
Скрябин Павел Николаевич <skryabin.p@coral-meat.ru>
2023-08-22 11:11:10 +0300

    Tasks update

---
+++ README.md
@@ -0,0 +1,13 @@
+# Get Weather Forecast 
+
+This is a little script for retrieving forecast from https://wttr.in/
+
+### About :
+It has function get_weather_forecast which takes City and preferable language as inputs and return
+forecast
+---
+Dependencies:
+- Requests
+
+
+
--- script.py
+++ script.py
@@ -2,12 +2,19 @@ import requests
 
 
 def get_weather_forecast(city: str, lang: str = 'ru'):
-    url = f'https://wttr.in/{city}?nTqmM&lang={lang}'
-    response = requests.get(url)
+    url = f'https://wttr.in/{city}'
+    payload = {"nTqmM": "","lang": lang}
+    response = requests.get(url,params=payload)
+    response.raise_for_status()
     return response.text
 
 
 if __name__ == '__main__':
-    print(get_weather_forecast('Лондон'))
-    print(get_weather_forecast('Шереметьево'))
-    print(get_weather_forecast('Череповец'))
+    cities_list = ['Лондон','Шереметьево','Череповец']
+    for city in cities_list:
+        try:
+            print(get_weather_forecast(city))
+        except requests.exceptions.HTTPError as error:
+            exit(f"Can't fetch the data from the remote host: \n {error}")
+
+
